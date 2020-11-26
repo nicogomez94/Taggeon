338b2c60659cecb6295f3299233b0ffb01b94b11
root <root@ip-172-31-27-77.us-east-2.compute.internal>
2020-11-26 22:24:27 +0000

    arreglos mis publicaciones

---
+++ frame/mis-publicaciones.php
@@ -0,0 +1,53 @@
+<?php
+include_once($GLOBALS['configuration']['path_app_admin_objects']."util/header.php");
+include_once($GLOBALS['configuration']['path_app_admin_objects']."publicacion/PublicacionManager.php");
+
+if ($perfil=='seller'){
+
+    $publicacionManager = new PublicacionManager();
+
+
+    $jsonData = array(
+        "usuario" => $GLOBALS['sesionG']['usuario'],
+        "nombre"      => $GLOBALS['sesionG']['nombre'],
+        "apellido"    => $GLOBALS['sesionG']['apellido'],
+        "contacto" => $GLOBALS['sesionG']['email'],
+        "categoria" => $publicacionManager->getListCategoria(),
+        "publicaciones"     => $publicacionManager->getListPublicacion()
+        
+    );
+    $jsonData = json_encode($jsonData);
+    $menuperfil = '';
+    if ($perfil == 'seller'){
+
+        $menuperfil = <<<STR
+        <a class="nav-item nav-link" href="/ampliar-publicacion.html">Mis Publicacions</a>
+STR;
+    }
+    $contenido = new Template($nameTemplate);
+	$contenido->asigna_variables(array(
+            "json" => $jsonData,
+            "usuario" => $GLOBALS['sesionG']['usuario'],
+            "nombre" => $GLOBALS['sesionG']['nombre']." ".$GLOBALS['sesionG']['apellido'],
+            "apellido" => $GLOBALS['sesionG']['apellido'],
+            "contacto" => $GLOBALS['sesionG']['email'],
+            "url_editar" => "/editar-usuario.html",
+            "menuperfil" => $menuperfil,
+            "foto-perfil" => $fotoPerfil //fotoPerfil definida en header.php
+			));
+    $contenidoString = $contenido->muestra();
+    //HEADER
+    echo $contenidoStringHeader;
+    //FIN HEADER
+	echo $contenidoString;
+}else{
+    //HEADER
+    echo $contenidoStringHeader;
+    //FIN HEADER
+   $contenidoString = Template::sostenedor_error("Permiso denegado {$perfil}");
+   echo $contenidoString;
+}
+Database::Connect()->close();
+exit;
+
+?>
