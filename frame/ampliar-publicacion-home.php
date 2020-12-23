4ed910a6518581f531a55df7b53067600d1230f5
root <root@ip-172-31-27-77.us-east-2.compute.internal>
2020-12-23 18:52:31 +0000

    arreglos

---
+++ frame/ampliar-publicacion-home.php
@@ -0,0 +1,53 @@
+<?php
+include_once($GLOBALS['configuration']['path_app_admin_objects']."util/header.php");
+include_once($GLOBALS['configuration']['path_app_admin_objects']."publicacion/PublicacionManager.php");
+include_once($GLOBALS['configuration']['path_app_admin_objects']."producto/ProductoManager.php");
+
+
+if ($perfil=='seller' || $perfil='picker'){
+
+    $publicacionManager = new PublicacionManager();
+    $productoManager = new ProductoManager();
+
+    $jsonData = array(
+	"perfil"        => $perfil,
+        "usuario"       => $GLOBALS['sesionG']['usuario'],
+        "nombre"        => $GLOBALS['sesionG']['nombre'],
+        "apellido"      => $GLOBALS['sesionG']['apellido'],
+        "contacto"      => $GLOBALS['sesionG']['email'],
+        "categoria"     => $publicacionManager->getListCategoria(),
+        "publicaciones"     => $publicacionManager->getListPublicacion(),
+        "categoria_producto" => $productoManager->getListCategoria(),
+        "rubro_producto"     => $productoManager->getListRubro(),
+        "productos"     => $productoManager->getListProducto()
+    );
+    $jsonData = json_encode($jsonData);
+    $menuperfil = '';
+    $idEditar = isset($_GET["id"]) ? $_GET["id"] : '';
+    $contenido = new Template($nameTemplate);
+	$contenido->asigna_variables(array(
+            "json" => $jsonData,
+            "usuario" => $GLOBALS['sesionG']['usuario'],
+            "nombre" => $GLOBALS['sesionG']['nombre']." ".$GLOBALS['sesionG']['apellido'],
+            "apellido" => $GLOBALS['sesionG']['apellido'],
+            "contacto" => $GLOBALS['sesionG']['email'],
+            "url_editar" => "/editar-usuario.html",
+	    "id"          => $idEditar,
+            "foto-perfil" => $fotoPerfil //fotoPerfil definida en header.php
+			));
+    $contenidoString = $contenido->muestra();
+    //HEADER
+    echo $contenidoStringHeader;
+    //FIN HEADER
+	echo $contenidoString;
+}else{
+    //HEADER
+    echo $contenidoStringHeader;
+    //FIN HEADER
+   $contenidoString = Template::sostenedor_error("Permiso denegado {$perfil}");
+   echo $contenidoString;
+}
+Database::Connect()->close();
+exit;
+
+?>
