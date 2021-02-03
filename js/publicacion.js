66df0b58058fadbd5bfc12db6d6646fd06cd689a
root <root@ip-172-31-27-77.us-east-2.compute.internal>
2021-02-03 03:55:22 +0000

    arreglos de imagenes en js y css. fix de null en carrito

--- js/publicacion.js
+++ js/publicacion.js
@@ -35,7 +35,6 @@ $(document).ready(function(){
                var objCatPrevId = objCatPrev.id || 0;
                $(".prev-cat a").attr("href",'/ampliar-publicacion-home.html?accion=ampliar&cat='+objCatPrevId);
 
-               console.log($(".up_relleno_2_izq"))
                //rellenos costados
                $(".up_relleno_1_der").html('<span class="inside_up_relleno">'+objCatNext.nombre+'</span>');
                $(".up_relleno_2_izq").html('<span class="inside_up_relleno">'+objCatPrev.nombre+'</span>');
@@ -87,15 +86,21 @@ $(document).ready(function(){
                               '</div>'+
                            '</div>'
                   
-                  for(var i=0; i<sizePublic; i++){
-                     var html_related = '<li class="splide__slide"><img data-toggle="modal" src="/productos_img/sdfs.jpg"></li>';
-                  }
+                  //dibujo splide con relacionados
+                  /*for(var i=0; i<sizePublic; i++){
+
+                     console.log("adentro for prod related")
+                     
+                        
+
+                     
+                  
+                  }*/
 
                            
             $(".insert-public").append(html_public);
         
-            
-        
+
             //imgperfil sacada del menu top
             var img_perfil = $(".img-perfil-usuario-drop").attr("src");
             $(".img-perfil-public img").attr("src", img_perfil);
@@ -104,140 +109,156 @@ $(document).ready(function(){
             //nombre perfil
             var nombre_perfil = $(".user-name-perfil").text();
             $(".title-public").html(nombre_perfil);
-            
-            //imagen principal de public
-            
-        
+
             ///DIBUJO PINES
             var producto_parse = JSON.parse(producto);
             var producto_parse_size = producto_parse.length;
 
             for(var x=0; x<producto_parse_size; x++){
-                var id_prod = producto_parse[x].name;
-                var coords = producto_parse[x].value;
-                var ycoord = coords.split("-")[0];
-                var xcoord = coords.split("-")[1];
-        
-                //checkeo si es el mismo id de tag y prod
-                var arr = jsonData.productos;
-                var obj = arr.find(o => o.id === id_prod);
-        
-                    if(id_prod == obj.id){
-        
-                        //le saco el index el producto correspondiente
-                        var arr2 = jsonData.productos;
-                        var index = arr2.findIndex(o => o.id === id_prod);
-                        //var test = Object.values(jsonData.productos)[x];
-            
-                        var nombre_prod = jsonData.productos[index].titulo;
-                        var precio_prod = jsonData.productos[index].precio;
-                        var marca_prod = jsonData.productos[index].marca;
-                        var color_prod = jsonData.productos[index].color;
-                        var descr_prod = jsonData.productos[index].descr_producto;
-                        var id_prod_json = jsonData.productos[index].id;
-                        var stock_prod = jsonData.productos[index].stock;
-                        var foto_prod = jsonData.productos[index].foto;
-                        var id_cat = jsonData.categoria[index].id;
-                        var nombre_completo = jsonData.nombre+""+jsonData.apellido;
-                        var foto_src_prod = '/productos_img/'+foto_prod+'.png';
+               var id_prod = producto_parse[x].name;
+               var coords = producto_parse[x].value;
+               var ycoord = coords.split("-")[0];
+               var xcoord = coords.split("-")[1];
+               var cat_actual_prod = "1";
+               var allprod = jsonData.productos || [];
+      
+               //checkeo si es el mismo id de tag y prod
+               var arr = jsonData.productos;
+               var obj = arr.find(o => o.id === id_prod);
 
-                            var modal_html =  
-                                '<div class="modal fade" id="modal-producto-'+id_prod+'" tabindex="-1" role="dialog" aria-labelledby="modal-producto-title" aria-hidden="true">'+
-                                '<div class="modal-dialog modal-dialog-centered modal-lg" role="document">'+
-                                '<div class="modal-content">'+
-                                '<!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button> -->'+
-                                '<div class="modal-body">'+
-                                '<div class="row">'+
-                                '<div class="col-lg-7">'+
-                                    '<div class="img-modal-prod"><img style="width: 100%;" src="'+foto_src_prod+'" alt="foto_src_prod"></div>'+
-                                '<hr>'+
-                                '<div>'+
-                                '<table class="tg" style="table-layout: fixed; width: 282px">'+
-                                '<colgroup>'+
-                                '<col style="width: 153px">'+
-                                '<col style="width: 129px">'+
-                                '</colgroup>'+
-                                '<tbody>'+
-                                '<tr>'+
-                                    '<td class="tg-9f3l">ID Producto</td>'+
-                                    '<td class="tg-wo29">'+id_prod_json+'</td>'+
-                                    '</tr>'+
-                                    '<tr>'+
-                                    '<td class="tg-9f3l">Marca</td>'+
-                                    '<td class="tg-wo29">'+marca_prod+'</td>'+
-                                    '</tr>'+
-                                    '<tr>'+
-                                    '<td class="tg-9f3l">Color</td>'+
-                                    '<td class="tg-wo29">'+color_prod+'</td>'+
-                                    '</tr>'+
-                                    '<tr>'+
-                                    '<td class="tg-9f3l">Categoria</td>'+
-                                    '<td class="tg-wo29">'+id_cat+'</td>'+
-                                    '</tr>'+
-                                    '<tr>'+
-                                    '<td class="tg-9f3l">Rubro</td>'+
-                                    '<td class="tg-z6p2">'+id_cat+'</td>'+//hardcodeado
-                                '</tr>'+
-                                '</tbody>'+
-                                '</table>'+
-                                '</div></div>'+
-                                '<div class="col-lg-5 col-datos-producto">'+
-                                '<div>'+
-                                    '<h2>'+nombre_prod+'</h2>'+
-                                    '<p style="font-size: 0.8em; color: grey; font-style: italic">Por: '+nombre_completo+'</p>'+
-                                '</div>'+
-                                '<hr>'+
-                                '<div>'+
-                                '<div class="precio-producto-modal"><span>$. '+precio_prod+'</span></div>'+
-                                '<div class="shipment-modal-producto">'+
-                                '<i class="fas fa-truck-loading"></i> Shipment dentro de las 5 dias habiles'+//hardcodeado
-                                '</div>'+
-                                '<hr>'+
-                                '<div class="stock-boton-modal">'+
-                                    '<span>'+
-                                        'Cantidad&nbsp;'+
-                                        '<select class="cantidad_value" name="cantidad">'+
-                                            '<option value="1">1</option>'+//hardcodeado
-                                            '<option value="2">2</option>'+//hardcodeado
-                                            '<option value="3">3</option>'+//hardcodeado
-                                            '<option value="4">4</option>'+//hardcodeado
-                                            '<option value="5">5</option>'+//hardcodeado
-                                            '<option value="6">6</option>'+//hardcodeado
-                                            '<option value="7">7</option>'+//hardcodeado
-                                        '</select>'+
-                                        '<input type="hidden" class="id_prod_carrito" name="id" value="'+id_prod_json+'">'+
-                                    '</span>&nbsp;'+
-                                    '<span><button class="btn btn-warning btn-carrito">Añadir a Carrito</button></span>'+
-                                '</div>'+
-                                '</div>'+
-                                '<hr>'+
-                                '<div class="descripcion-modal-producto">'+
-                                '<strong>Descripcion:</strong>'+
-                                '<div>'+descr_prod+'</div>'+
-                                '</div>'+
-                                '<hr>'+
-                                '</div></div></div></div></div></div>';
-            
-                            $("body").append(modal_html);
-                        
-                                //dibujo splide
-                        
-                    }//fin if prod
-                
-                    var splide_fotos = '<li class="splide__slide"><img data-toggle="modal" data-target="#modal-producto-'+id_prod+'" src="'+foto_src_prod+'"></li>';
-                    $(".splide__list__"+i).append(splide_fotos);
-                //dibujo tags
-                var tag_html = '<div class="tagg tagg-'+id_prod+'" style="top:'+ycoord+'; left: '+xcoord+'">'+
-                            '<span><i class="fas fa-tags"></i></span></div>';
-                
-                $(".tag-container-"+i).append(tag_html);
+               //checkeo que cat es para mostrar relacionados
+               //var objRel = arr.find(o => o.id === cat_actual);
+                  //dibujo modales
+                  if(id_prod == obj.id){
+      
+                     //le saco el index el producto correspondiente
+                     var arr2 = jsonData.productos;
+                     var index = arr2.findIndex(o => o.id === id_prod);
+                     //var test = Object.values(jsonData.productos)[x];
+         
+                     var nombre_prod = jsonData.productos[index].titulo;
+                     var precio_prod = jsonData.productos[index].precio;
+                     var marca_prod = jsonData.productos[index].marca;
+                     var color_prod = jsonData.productos[index].color;
+                     var descr_prod = jsonData.productos[index].descr_producto;
+                     var id_prod_json = jsonData.productos[index].id;
+                     var stock_prod = jsonData.productos[index].stock;
+                     var foto_prod = jsonData.productos[index].foto;
+                     var id_cat = jsonData.categoria[index].id;
+                     var nombre_completo = jsonData.nombre+""+jsonData.apellido;
+                     var foto_src_prod = '/productos_img/'+foto_prod+'.png';
 
-                //click en tag
-                $(".bodyimg-public-container-"+i).on("click", ".tagg", function(){
-                    var prod_public = $(this).parent().parent().find(".productos-public");
-                    prod_public.toggle(100);
-                    //data-toggle="modal" data-target="#modal-producto-'+id_prod+'"
-                });
+                           var modal_html =  
+                              '<div class="modal fade" id="modal-producto-'+id_prod+'" tabindex="-1" role="dialog" aria-labelledby="modal-producto-title" aria-hidden="true">'+
+                              '<div class="modal-dialog modal-dialog-centered modal-lg" role="document">'+
+                              '<div class="modal-content">'+
+                              '<!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button> -->'+
+                              '<div class="modal-body">'+
+                              '<div class="row">'+
+                              '<div class="col-lg-7">'+
+                                 '<div class="img-modal-prod"><img style="width: 100%;" src="'+foto_src_prod+'" alt="foto_src_prod"></div>'+
+                              '<hr>'+
+                              '<div>'+
+                              '<table class="tg" style="table-layout: fixed; width: 282px">'+
+                              '<colgroup>'+
+                              '<col style="width: 153px">'+
+                              '<col style="width: 129px">'+
+                              '</colgroup>'+
+                              '<tbody>'+
+                              '<tr>'+
+                                 '<td class="tg-9f3l">ID Producto</td>'+
+                                 '<td class="tg-wo29">'+id_prod_json+'</td>'+
+                                 '</tr>'+
+                                 '<tr>'+
+                                 '<td class="tg-9f3l">Marca</td>'+
+                                 '<td class="tg-wo29">'+marca_prod+'</td>'+
+                                 '</tr>'+
+                                 '<tr>'+
+                                 '<td class="tg-9f3l">Color</td>'+
+                                 '<td class="tg-wo29">'+color_prod+'</td>'+
+                                 '</tr>'+
+                                 '<tr>'+
+                                 '<td class="tg-9f3l">Categoria</td>'+
+                                 '<td class="tg-wo29">'+id_cat+'</td>'+
+                                 '</tr>'+
+                                 '<tr>'+
+                                 '<td class="tg-9f3l">Rubro</td>'+
+                                 '<td class="tg-z6p2">'+id_cat+'</td>'+//hardcodeado
+                              '</tr>'+
+                              '</tbody>'+
+                              '</table>'+
+                              '</div></div>'+
+                              '<div class="col-lg-5 col-datos-producto">'+
+                              '<div>'+
+                                 '<h2>'+nombre_prod+'</h2>'+
+                                 '<p style="font-size: 0.8em; color: grey; font-style: italic">Por: '+nombre_completo+'</p>'+
+                              '</div>'+
+                              '<hr>'+
+                              '<div>'+
+                              '<div class="precio-producto-modal"><span>$. '+precio_prod+'</span></div>'+
+                              '<div class="shipment-modal-producto">'+
+                              '<i class="fas fa-truck-loading"></i> Shipment dentro de las 5 dias habiles'+//hardcodeado
+                              '</div>'+
+                              '<hr>'+
+                              '<div class="stock-boton-modal">'+
+                                 '<span>'+
+                                       'Cantidad&nbsp;'+
+                                       '<select class="cantidad_value" name="cantidad">'+
+                                          '<option value="1">1</option>'+//hardcodeado
+                                          '<option value="2">2</option>'+//hardcodeado
+                                          '<option value="3">3</option>'+//hardcodeado
+                                          '<option value="4">4</option>'+//hardcodeado
+                                          '<option value="5">5</option>'+//hardcodeado
+                                          '<option value="6">6</option>'+//hardcodeado
+                                          '<option value="7">7</option>'+//hardcodeado
+                                       '</select>'+
+                                       '<input type="hidden" class="id_prod_carrito" name="id" value="'+id_prod_json+'">'+
+                                 '</span>&nbsp;'+
+                                 '<span><button class="btn btn-warning btn-carrito">Añadir a Carrito</button></span>'+
+                              '</div>'+
+                              '</div>'+
+                              '<hr>'+
+                              '<div class="descripcion-modal-producto">'+
+                              '<strong>Descripcion:</strong>'+
+                              '<div>'+descr_prod+'</div>'+
+                              '</div>'+
+                              '<hr>'+
+                              '</div></div></div></div></div></div>';
+         
+                           $("body").append(modal_html);
+                     
+                              //dibujo splide
+                     
+                  }/*else if(cat_actual_prod == "1"){
+                     console.log("andreon if")
+                     
+                     
+                  }*///fin ifs prod
+                
+                  var splide_fotos = '<li class="splide__slide"><img data-toggle="modal" data-target="#modal-producto-'+id_prod+'" src="'+foto_src_prod+'"></li>';
+                  $(".splide__list__"+i).append(splide_fotos);
+
+                  //related (por ahora traigo todos los prod)
+                  for(var y=0; y<allprod.length; y++){
+                     console.log("dentro for")
+                     var foto_prod_rel = jsonData.productos[y].foto;
+                     var foto_src_prod_rel = '/productos_img/'+foto_prod_rel+'.png';
+                     var html_related = '<li class="splide__slide"><img data-toggle="modal" src="'+foto_src_prod_rel+'"></li>';
+                     $(".splide_list_related").append(html_related);
+                  }
+            
+                  //dibujo tags
+                  var tag_html = '<div class="tagg tagg-'+id_prod+'" style="top:'+ycoord+'; left: '+xcoord+'">'+
+                              '<span><i class="fas fa-tags"></i></span></div>';
+                  
+                  $(".tag-container-"+i).append(tag_html);
+
+                  //click en tag
+                  $(".bodyimg-public-container-"+i).on("click", ".tagg", function(){
+                     var prod_public = $(this).parent().parent().find(".productos-public");
+                     prod_public.toggle(100);
+                     //data-toggle="modal" data-target="#modal-producto-'+id_prod+'"
+                  });
           
             
             }//fin for prdo
@@ -371,7 +392,7 @@ $("#finalizar-orden").submit(function(){
               //window.location.replace(dataM);														
            }else if(dataJ == 'OK'){
               alert("OK-->"+dataJ+"/"+dataM);
-              window.location.replace("/cobrar-compra.html?id="+id_carrito);
+              window.location.replace("/cobrar-compra.html");
            }else{
               alert("ELSE-->"+dataJ+"/"+dataM);
               //window.location.replace("/ampliar-carrito.html");
@@ -385,7 +406,7 @@ $("#finalizar-orden").submit(function(){
     return false;
 });
 
-///finalizar orden
+///realizar/finalizar compra
 $("#realizar-compra").submit(function(){
 
    var id_carrito = jsonData.carrito[0].id_carrito;
@@ -409,7 +430,7 @@ $("#realizar-compra").submit(function(){
              //window.location.replace(dataM);														
           }else if(dataJ == 'OK'){
              alert("OK-->"+dataJ+"/"+dataM);
-             window.location.replace("/cobrar-compra.html?id="+id_carrito);
+             window.location.replace("/");
           }else{
              alert("ELSE-->"+dataJ+"/"+dataM);
              //window.location.replace("/ampliar-carrito.html");
