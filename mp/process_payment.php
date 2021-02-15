d0aa3a20356e4e474dbbfbd203affa388d41e01b
Ubuntu <ubuntu@ip-172-31-27-77.us-east-2.compute.internal>
2021-02-15 22:18:15 +0000

    primer etapa mercadopago

---
+++ mp/process_payment.php
@@ -0,0 +1,31 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("TEST-1987948042281001-021502-62da6d66e60287be7953ab40b959d5cf-22602582");
+
+    $payment = new MercadoPago\Payment();
+    $payment->transaction_amount = (float)$_POST['transactionAmount'];
+    $payment->token = $_POST['token'];
+    $payment->description = $_POST['description'];
+    $payment->installments = (int)$_POST['installments'];
+    $payment->payment_method_id = $_POST['paymentMethodId'];
+    $payment->issuer_id = (int)$_POST['issuer'];
+
+    $payer = new MercadoPago\Payer();
+    $payer->email = $_POST['email'];
+    $payer->identification = array(
+        "type" => $_POST['docType'],
+        "number" => $_POST['docNumber']
+    );
+    $payment->payer = $payer;
+
+    $payment->save();
+
+    $response = array(
+        'status' => $payment->status,
+        'status_detail' => $payment->status_detail,
+        'id' => $payment->id
+    );
+    echo json_encode($response);
+
+?>
